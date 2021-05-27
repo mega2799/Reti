ff7155ec7cd0658f576b433c5aab0622756aac18
mega <megadigimatt@gmail.com>
2021-05-27 10:39:38 +0200

    now gateway takes the messages from the devices and makes them a list

--- gatewayServer.py
+++ gatewayServer.py
@@ -2,6 +2,8 @@ import socket as sk
 
 import time
 
+DEBUG = False 
+
 sock = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
 
 server_address = ('localhost', 9898)
@@ -11,11 +13,15 @@ print ('[G] gateway starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 while True:
-#    print('[G] gateway waiting to receive message from the devices')
+    if DEBUG:
+        print('[G] gateway waiting to receive message from the devices')
 
     data, address = sock.recvfrom(4096)
 
-    print('\n**[G]** received %s bytes from %s' % (len(data), address))
+    listData = data.decode('utf-8').split(" ")
 
-    print(data.decode('utf8') + "\n")
+    if DEBUG:
+        print('\n**[G]** received %s bytes from %s' % (len(data), address))
+
+        print(listData)
 
