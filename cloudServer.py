5f8d9a0fc904fd4f43a670f59e3e36354a4b2dc8
mega2799 <megadigimatt@gmail.com>
2021-06-20 11:10:24 +0200

    Now prints buffer dimension, better print for readable data

--- cloudServer.py
+++ cloudServer.py
@@ -2,13 +2,13 @@ import sys
 
 from socket import * 
 
-import tkinter as tk
-
 from IOTdevice import Device
 
 from datetime import datetime
 
 DEBUG = False
+
+BufferDIM = 1048 
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -18,7 +18,8 @@ serverSocket.bind(server_address)
 
 serverSocket.listen(1)
 
-print ('[C] the cloud server is listening up on port:',8989)
+print("[C] the cloud server is listening up on port:",8989)
+print("[C] Buffer dimension : ", BufferDIM)
 
 tmp = Device("tempDevice", "local")
 
@@ -31,25 +32,22 @@ while True:
     try:
         if DEBUG:
             print(" [C] I got a message! \n")
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(BufferDIM)
         if len(message) > 0 and DEBUG : 
             print(message.split())
         
         dataList = [elem.decode('utf-8') for elem in message.split()]
         
-        #[print(Device.autentication(dataList[k], dataList[k+3], dataList[k +4], dataList[k+5])) for k in dataList]
-
-        #print("Name ----------------- Time --------------- Humidity --------------- Temperature")
         for k in range(0,len(dataList),6):
             print("-----------------------------------------------------------------------------------")   
-            print("Name  |        Date      |       Time  |         Humidity  |   Temperature")
+            print("Name\t|\tIP\t|\tDate\t|\tTime\t|\tTemperature\t|\tHumidity\t|")
             actual = datetime.strptime(datetime.now().strftime("%H:%M:%S.%f"), "%H:%M:%S.%f")
             
-            print(tmp.autentication(dataList[k], dataList[k+2], dataList[k+3], dataList[k +4], dataList[k+5]))
+            print(tmp.autentication(dataList[k], dataList[k+1], dataList[k+2], dataList[k+3], dataList[k +4], dataList[k+5]))
 
             delay = (actual - datetime.strptime(dataList[k+3], "%H:%M:%S.%f")).total_seconds()
 
-            print(f'\n il device {dataList[k]} ha {delay= }')
+            print(f'\n il device {dataList[k]} ha in totale {delay= } s')
 
     except IOError:
         print("Error Occurred")
